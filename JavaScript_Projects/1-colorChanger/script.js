3b0c8e82567fdcca8cc08fb0ae33139b392b471b
vipulpahival <vipulpahiwal59@gmail.com>
2026-06-04 16:50:43 +0530

    add 1-colorChanger Project

---
+++ JavaScript_Projects/1-colorChanger/script.js
@@ -0,0 +1,31 @@
+const btns = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+btns.forEach((btn)=>{
+
+btn.addEventListener('click', function(e){
+       if(e.target.id=== 'grey'){
+        body.style.backgroundColor = e.target.id;
+       }
+
+              if(e.target.id=== 'white'){
+        body.style.backgroundColor = e.target.id;
+       }
+
+              if(e.target.id=== 'blue'){
+        body.style.backgroundColor = e.target.id;
+       }
+
+              if(e.target.id=== 'yellow'){
+        body.style.backgroundColor = e.target.id;
+       }
+
+         if(e.target.id=== 'purple'){
+        body.style.backgroundColor = e.target.id;
+       }
+    
+   
+})
+
+
+});
